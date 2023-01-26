2988733c70e87cb746007781c0131ab72191dfee
Tesfatsion Shiferaw <62855279+Tesfa-eth@users.noreply.github.com>
2023-01-26 18:22:46 -0500

    all functionalities working

--- src/App.jsx
+++ src/App.jsx
@@ -1,97 +1,152 @@
-import React, {useState} from 'react';
-import Header from './components/Header';
-import OriginLists from './components/OriginLists';
-import Graph from './components/Graph';
-import './App.css';
-import Loading from './components/Loading';
+import React, { useState } from "react";
+import Header from "./components/Header";
+import OriginLists from "./components/OriginLists";
+import Graph from "./components/Graph";
+import "./App.css";
+import Loading from "./components/Loading";
 
 export default function App() {
   const [nameOrigins, setNameOrigins] = useState([]);
-  const [searchedName, setSearchedName] = useState('');
+  const [searchedName, setSearchedName] = useState("");
   const [showMore, setShowMore] = useState(false);
   const [darkMode, setDarkMode] = useState(false);
-  const [labelsState, setLabelsState] = useState([])
-  const [loadingCountryName, setLoadingCountryName] = useState(false)
+  const [labelsState, setLabelsState] = useState([]);
+  const [loadingCountryName, setLoadingCountryName] = useState(false);
 
-  const getCountryName = (countrycodes) =>{
-    setLoadingCountryName(true)
+  const getNameOriginss = async (url) => {
+    const response = await fetch(url)
+      .then((response) => response.json())
+      .catch((err) => console.log(err));
+
+    setNameOrigins(response.country);
+    response.country.map(async (country, id) => {
+      await getCountryNamee(country.country_id);
+    });
+  };
+
+  const getCountryNamee = (countrycode) => {
+    return fetch(`https://restcountries.com/v3.1/alpha/${countrycode}`)
+      .then((data) => data.json())
+      .then((data) => {
+        //console.log(data);
+        setLabelsState((labelsState) => [...labelsState, data]);
+        // setLoadingCountryName(false);
+      });
+  };
+
+  const getCountryName = (countrycodes) => {
+    setLoadingCountryName(true);
     for (let i = 0; i < countrycodes.length; i++) {
       //XMLHttpRequest to make sych requests to get full country name from nationalize.io
       const xhr = new XMLHttpRequest();
-      xhr.open("GET", `https://restcountries.com/v3.1/alpha/${countrycodes[i].country_id}`, [false]);
-      xhr.send()
-      xhr.onload = function() {
-       const countryName = JSON.parse(xhr.response)[0].name.common
-       setLabelsState(labelsState => [...labelsState, countryName]);
+      xhr.open(
+        "GET",
+        `https://restcountries.com/v3.1/alpha/${countrycodes[i].country_id}`,
+        [false]
+      );
+      xhr.send();
+      xhr.onload = function () {
+        const countryName = JSON.parse(xhr.response)[0].name.common;
+        setLabelsState((labelsState) => [...labelsState, countryName]);
       };
-  }
-  setLoadingCountryName(false)
-  }
+    }
+    setLoadingCountryName(false);
+  };
   const getNameOrigins = (url) => {
     fetch(url)
-    .then(response => response.json())
-    .then(
-      data => {
-        setNameOrigins(data.country)
-        getCountryName(data.country)
-      })
-  }
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data.country);
+        setNameOrigins(data.country);
+        getCountryName(data.country);
+      });
+  };
 
   const handleChange = (e) => {
-    setSearchedName(e.target.value)
-    e.preventDefault()
-  }
+    setSearchedName(e.target.value);
+    e.preventDefault();
+  };
 
-  const toggleDarkMode = () =>{
-    setDarkMode(!darkMode)
-  }
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
 
-  const toggleShowMore = () =>{
-    setShowMore(!showMore)
-  }
+  const toggleShowMore = () => {
+    setShowMore(!showMore);
+  };
 
   const handleSubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     //console.log(labelsState)
-    setLabelsState(labelsState => []);
-    var url = `https://api.nationalize.io/?name=${searchedName}`
-    setShowMore(false)
-    getNameOrigins(url)
-  }
+    setLabelsState((labelsState) => []);
+    var url = `https://api.nationalize.io/?name=${searchedName}`;
+    setShowMore(false);
+    //getNameOrigins(url)
+    getNameOriginss(url);
+  };
 
   return (
-    <div className={darkMode ? 'dark': ""}>
-      <div className=' flex flex-col justify-between bg-gradient-to-b from-teal-900 to-teal-800 min-h-screen dark:bg-gray-700'>
-      <div>
-      <Header toggle={toggleDarkMode}/>
-      <div className='form-wrapper'>
-        <form onSubmit={handleSubmit} id="form">
-            <label>
-            <input onChange={handleChange} value={searchedName} className="form-control" id="title" name='namesearched' type="text" placeholder='Search name here'/>
-            </label>
-            <input className='text-white bg-teal-600 ml-2 p-2 rounded hover:bg-teal-500' type="submit" value="Look up" />
-        </form>
-      </div>
+    <div className={darkMode ? "dark" : ""}>
+      <div className=" flex flex-col justify-between bg-gradient-to-b from-teal-900 to-teal-800 min-h-screen dark:bg-gray-700">
+        <div>
+          <Header toggle={toggleDarkMode} />
+          <div className="form-wrapper">
+            <form onSubmit={handleSubmit} id="form">
+              <label>
+                <input
+                  onChange={handleChange}
+                  value={searchedName}
+                  className="form-control"
+                  id="title"
+                  name="namesearched"
+                  type="text"
+                  placeholder="Search name here"
+                />
+              </label>
+              <input
+                className="text-white bg-teal-600 ml-2 p-2 rounded hover:bg-teal-500"
+                type="submit"
+                value="Look up"
+              />
+            </form>
+          </div>
 
-      { loadingCountryName ? 
-       <Loading loading={loadingCountryName} />:
-      <OriginLists nameOrigins={nameOrigins} showMoreToggle={toggleShowMore} showMore={showMore} label={labelsState} loadingCountryName={loadingCountryName}/> 
-        }
-      <div className=' mt-5 flex flex-col items-center'>
-      {showMore ? <Graph nameOrigins={nameOrigins} labelsState={labelsState}/> : ""}
-      </div>
-      </div>
+          {loadingCountryName ? (
+            <Loading loading={loadingCountryName} />
+          ) : (
+            <OriginLists
+              nameOrigins={nameOrigins}
+              showMoreToggle={toggleShowMore}
+              showMore={showMore}
+              label={labelsState}
+              loadingCountryName={loadingCountryName}
+            />
+          )}
+          <div className=" mt-5 flex flex-col items-center">
+            {showMore ? (
+              <Graph nameOrigins={nameOrigins} labelsState={labelsState} />
+            ) : (
+              ""
+            )}
+          </div>
+        </div>
 
-
-      <div>
-        <footer>
-        <hr className=" border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-6" />
-        <span className="block text-sm text-white ml-3 sm:text-center ">© 2022 <a href="https://tesfatsionshiferaw.netlify.app/" className="hover:text-wh">Tesfatsion Shiferaw™</a>. All Rights Reserved.
-        </span>
-        </footer>
-      </div>
-
+        <div>
+          <footer>
+            <hr className=" border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-6" />
+            <span className="block text-sm text-white ml-3 sm:text-center ">
+              © 2022{" "}
+              <a
+                href="https://tesfatsionshiferaw.netlify.app/"
+                className="hover:text-wh"
+              >
+                Tesfatsion Shiferaw™
+              </a>
+              . All Rights Reserved.
+            </span>
+          </footer>
+        </div>
       </div>
     </div>
-  )
+  );
 }
--- src/components/Graph.jsx
+++ src/components/Graph.jsx
@@ -1,65 +1,70 @@
-import React from 'react'
+import React from "react";
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
-import { Pie} from "react-chartjs-2";
-import { useState } from 'react';
-import '../App.css';
+import { Pie } from "react-chartjs-2";
+import { useState } from "react";
+import "../App.css";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
-export default function Graph({nameOrigins, labelsState}) {
-    const labels = []
-    //const [labelsState, setLabelState] = useState([])
-    const datapoints = []
-    const [countryName, setCountryName] = useState('')
-    //getCountry1({setCountryName});
-    //console.log(countryName)
-    const dataCleanUp = (dataInput) => {
-      dataInput.map((data, index)=>{
+export default function Graph({ nameOrigins, labelsState }) {
+  //console.log("Graph", labelsState);
+  const labels = [];
+  //const [labelsState, setLabelState] = useState([])
+  const datapoints = [];
+  const [countryName, setCountryName] = useState("");
+  //getCountry1({setCountryName});
+  //console.log(countryName)
+  const combinedArray = labelsState.reduce((acc, current, index) => {
+    return [...acc, [current, nameOrigins[index]]];
+  }, []);
+  console.log(combinedArray);
+  const dataCleanUp = (dataInput) => {
+    dataInput.map((data, index) => {
       // getCountryName(data.country_id).then(data => {
       //   labels.push(data)
       // });
-      labels.push(data.country_id)
-      datapoints.push(data.probability*100)
-    })
+      labels.push(data[0][0].name.common);
+      datapoints.push(data[1].probability * 100);
+    });
     //console.log(labels.country_id);
-    }
-    dataCleanUp(nameOrigins)
-    //loadCountryNames(labels)
-    const data = {
-      labels: labelsState,
-      datasets: [
-        {
-          label: 'probability of name origin %',
-          data: datapoints,
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)',
-          ],
-          borderColor: [
-            'rgba(255, 99, 132, 1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)',
-          ],
-          borderWidth: 1,
-        },
-      ],
-    }
-    
-    const options = {
-      maintainAspectRatio: false	// Don't maintain w/h ratio
-    }
+  };
+  dataCleanUp(combinedArray);
+  //loadCountryNames(labels)
+  const data = {
+    labels: labels,
+    datasets: [
+      {
+        label: "probability of name origin %",
+        data: datapoints,
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const options = {
+    maintainAspectRatio: false, // Don't maintain w/h ratio
+  };
 
   return (
     <div>
-        <article className="canvas-container">
-              <Pie data={data} options={options}/>
-        </article>
+      <article className="canvas-container">
+        <Pie data={data} options={options} />
+      </article>
     </div>
-  )
+  );
 }
